29249c0c5125b7d9c6b15f8479570720fef7e235
Rajat Sharma <rajatxd007@gmail.com>
2023-06-18 15:05:05 +0530

    Basic Setup for API Gateway Service

--- src/routes/v1/booking-routes.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-
-const  router = express.Router();
-const {BookingController}  = require('../../controllers'); 
-
-router.post('/', BookingController.createBooking);
-router.post('/payments', BookingController.updatePayment);
-module.exports = router;
