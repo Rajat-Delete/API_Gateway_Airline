b36776d17d0c5ebf3caa6e26106d8a324fdc5778
Rajat Sharma <rajatxd007@gmail.com>
2023-06-19 23:20:08 +0530

    Added Encryption in SignUp API

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -4,5 +4,6 @@ dotenv.config();
 
 module.exports = {
     PORT: process.env.PORT,
-    FLIGHT_SERVICE_URL : process.env.FLIGHT_SERVICE_URL
+    FLIGHT_SERVICE_URL : process.env.FLIGHT_SERVICE_URL,
+    SALT_ROUNDS : process.env.SALT_ROUNDS
 }
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,9 @@
 const {
   Model
 } = require('sequelize');
+
+const bcrypt = require('bcrypt');
+const {ServerConfig} = require('../config');
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
     /**
@@ -33,5 +36,14 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'user',
   });
+
+  user.beforeCreate(function encrypt(user){
+    console.log('user object before encryption',user);
+    //We have to pass password and the Salt for unique encrytpion hash
+    const encryptedPassword = bcrypt.hashSync(user.password , +ServerConfig.SALT_ROUNDS);//ServerConfig.SALT_ROUNDS was a string so we have converted it into number by +
+    user.password = encryptedPassword;
+    console.log('user object after encryption',user);
+  })
+
   return user;
 };
